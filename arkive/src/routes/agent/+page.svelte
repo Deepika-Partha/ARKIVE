f7fa2b3123743bceca3c9666c3cc58dc70043a40
Deepika <deepikaps26@gmail.com>
2025-05-22 11:13:54 -0400

    main links barebones set up

---
+++ arkive/src/routes/agent/+page.svelte
@@ -0,0 +1,11 @@
+<!-- AGENT PAGE -->
+
+<div class="hero">
+  <div class="left">
+    <h1>Agent Page</h1>
+    <p>  - Document Upload </p>
+    <p>  - With documents uploaded include chatbot in the corner that the user can ask questions about the uploaded documents to </p>
+  </div>
+</div>
+
+
---
+++ arkive/src/routes/calendar/+page.svelte
@@ -0,0 +1,12 @@
+<!-- CALENDAR PAGE -->
+
+<div class="hero">
+  <div class="left">
+    <h1>Calendar Page</h1>
+    <p>  - Calendar </p>
+    <p>  - Highlight current date </p>
+    <p>  - Monthly view </p>
+    <p>  - Add schedule and important dates</p>
+    <p>  - Color coded </p>
+  </div>
+</div>
